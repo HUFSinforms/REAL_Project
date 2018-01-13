3e5026f3fcc2050dfed2d9a6dab516a626f96951
KangDaeMook <eoanr1237@naver.com>
2018-01-13 14:14:43 +0900

    ay

--- CPLEX_NEW.py
+++ CPLEX_NEW.py
@@ -6,7 +6,7 @@ import time
 
 
 class portfolio():
-    def __init__(self, sector, bench, asset, MCAPQ, beta, alpha):
+    def __init__(self, sector, bench, asset, MCAPQ, beta, alpha,w_pre,O_scale):
         self.pluschek = 0
         self.w_upchek = 0
         self.q1num = 0
@@ -17,19 +17,54 @@ class portfolio():
         self.bench = bench
 
         self.c.variables.add(names=["d" + str(i) for i in asset], obj=alpha, lb=[-1 * bench[j] for j in asset])
+        
+        
 
         self.c.variables.add(names=["assum"], lb=[-99999])
+        
+        
+        
+        self.c.variables.add(names=["O"], obj=[O_scale], lb=[-99999])
+        
+
+        # print(c.variables.get_lower_bounds())
+
+
+        # c.variables.add(names=["y"+str(i) for i in asset],lb=[-99999999 for j in asset])
+        #
+        # c.variables.add(names=["c"+ str(j) + str(i) for j in asset for i in range(2)], types=[t.binary for i in asset for j in range(2)])
+
+
+
+        # c.linear_constraints.add(
+        #     lin_expr=[cplex.SparsePair(ind=["assum"], val=[1.0])], senses=["E"],
+        #     rhs=[returns], names=["sum"])
 
         for i in asset:
             self.c.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=["d" + str(i)], val=[1.0])], senses=["G"],
                                           rhs=[-1 * bench[i]], names=[str(i) + "di_wi"])
+            # c.linear_constraints.set_linear_components(str(i)+"di_wi", [["w" + str(i)], [1.0]])
             self.c.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=["d" + str(i)], val=[1.0])], senses=["L"],
                                           rhs=[0.05], names=["st_5_1"])
             self.c.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=["d" + str(i)], val=[1.0])], senses=["G"],
                                           rhs=[-0.05], names=["st_5_2"])
+            
+           
 
-    
+        #
+        # bench_sum = 0
+        #
+        # for i in bench:
+        #     bench_sum += bench[i]
+
+        # print(bench_sum)
+
         self.c.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = ["d"+str(i) for i in asset], val = [1.0]*len(asset))],senses=["E"],rhs=[0],names=["st_4"])
+        
+        
+        self.c.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = ["d"+str(i) for i in asset], val = [-1.0]*len(asset))],senses=["E"],rhs=[1-w_pre],names=["TurnOver"])
+        
+        self.c.linear_constraints.set_linear_components("TurnOver", [["O"], [1.0]])
 
         for j in sector:
             self.c.linear_constraints.add(
@@ -73,13 +108,16 @@ class portfolio():
             rhs=[0.0], names=["sum"])
         self.c.linear_constraints.set_linear_components("sum", [["assum"], [-1.0]])
 
+        # 0.112046486647
+        # -0.07957269716079549
+
         bigeer_bench_sum = 0
         for i in self.asset:
             bigeer_bench_sum = bigeer_bench_sum + self.bench[i] * big_w_dic[i]
 
         self.c.linear_constraints.add(
             lin_expr=[cplex.SparsePair(ind=["d" + str(i) for i in self.asset], val=[big_w_dic[i] for i in self.asset])],
-            senses=["E"],
+            senses=["G"],
             rhs=[w_upsums - bigeer_bench_sum], names=["w_big"])
 
         self.w_upchek += 1
@@ -105,12 +143,48 @@ class portfolio():
 
 
 
+
+
+
+
+            # c.linear_constraints.add(
+            #     lin_expr=[cplex.SparsePair(ind=["q" + str(i) for i in asset], val=[1.0]*len(asset))], senses=["G"],
+            #     rhs=[50], names=["st_9_1"])
+            #
+            #
+            # c.linear_constraints.add(
+            #     lin_expr=[cplex.SparsePair(ind=["q" + str(i) for i in asset], val=[1.0]*len(asset))], senses=["L"],
+            #     rhs=[70], names=["st_9_2"])
+
+            # c.linear_constraints.add(
+            #     lin_expr=[cplex.SparsePair(ind=["y" + str(i) for i in asset], val=[1.0] * len(asset))], senses=["G"],
+            #     rhs=[0], names=["st_10_1"])
+            #
+            # c.linear_constraints.add(
+            #     lin_expr=[cplex.SparsePair(ind=["y" + str(i) for i in asset], val=[1.0] * len(asset))], senses=["L"],
+            #     rhs=[0.4], names=["st_10_2"])
+
+
+
+            # for i in asset:
+            #     Q = cplex.SparseTriple(ind1=["q"+str(i)], ind2=["d"+str(i),"z"+str(i)],
+            #                        val=[-1.0,1.0])
+            #     c.quadratic_constraints.add(rhs=bench[i], quad_expr=Q, name="Q"+str(i))
+            # #
+            # Q2 = cplex.SparseTriple(ind1=["q" + str(i) for i in asset], ind2=["d" + str(i) for i in asset],
+            #                        val=[-1.0]*len(asset))
+            # c.quadratic_constraints.add(rhs=0.6, quad_expr=Q2, name="st_10_2", sense="G")
+
     def quard_con(self,Q_con, multiple):
         if self.q1num > 0:
             self.c.quadratic_constraints.delete("st_11_1")
         Q3 = cplex.SparseTriple(ind1=Q_con[0], ind2=Q_con[1], val=Q_con[2])
         self.c.quadratic_constraints.add(rhs=0.01 * multiple, quad_expr=Q3, name="st_11_1", sense="L")
         self.q1num += 1
+
+    # c.linear_constraints.add(
+    #     lin_expr=[cplex.SparsePair(ind=["z" + str(i) for i in asset], val=[-1.0 for i in asset])], senses=["G"],
+    #     rhs=[-0.4], names=["st_10_2"])
 
 
     def quard_obj(self,qmat):
@@ -133,16 +207,28 @@ class portfolio():
         abcd = []
         ab = {}
         cd = {}
+        ef={}
 
         numa = 0
         for i in self.asset:
+
+            # print("d" + str(i)  + " : " + str(c.solution.get_values("d" + str(i))))
+            # if self.bench[i] + self.c.solution.get_values("d" + str(i)) > 0:
+            # print("w" + str(i)  + " : " + str(bench[i] + self.c.solution.get_values("d" + str(i))))
+            # print(numa)
+            # print("q" + str(i) + " : " + str(c.solution.get_values("q" + str(i))))
             ab.update({str(i): self.bench[i] + self.c.solution.get_values("d" + str(i))})
 
             cd.update({str(i): self.c.solution.get_values("d" + str(i))})
+            
+            ef.update({str(i):self.c.solution.get_reduced_costs("d" + str(i))})
             numa += 1
-       
+            #    print("assum"+ " : " + str( c.solution.get_values("assum" )))
+            #    print(c.solution.get_objective_value())
+            #    print(c.solution.get_quadratic_slacks())
         abcd.append(ab)
         abcd.append(cd)
         abcd.append(self.c.solution.get_objective_value())
+        abcd.append(ef)
 
         return abcd
--- class_informs.py
+++ class_informs.py
@@ -6,7 +6,7 @@ import CPLEX_NEW as cn
 
 
 class informs:
-    def __init__(self,omega_multi):
+    def __init__(self,omega_multi,alpha_multi,w_pre,O_scale):
         f0 = open('pic/dic_sector.txt', 'rb')
         f1 = open('pic/dic_bench.txt', 'rb')
         f2 = open('pic/risk_sedol.txt', 'rb')
@@ -30,6 +30,9 @@ class informs:
         q_con1 = []
         q_con2 = []
         q_val = []
+        
+        for i in range(len(self.alpha)):
+            self.alpha[i] = self.alpha[i]/10000*alpha_multi
 
         for i in range(len((self.risk_mat[0]))):
             for j in range(len(self.risk_mat[0])):
@@ -48,9 +51,17 @@ class informs:
         Q_cons.append(q_con2)
         Q_cons.append(q_val)
         self.Q_con = Q_cons
-        self.cplexs = cn.portfolio(sector=self.dic_sector, bench=self.dic_bench, asset=self.risk_sedol, MCAPQ=self.dic_MCAP, beta=self.dic_beta,alpha=self.alpha)
+
+        self.cplexs = cn.portfolio(sector=self.dic_sector, bench=self.dic_bench, asset=self.risk_sedol, MCAPQ=self.dic_MCAP, beta=self.dic_beta,alpha=self.alpha,w_pre=w_pre,O_scale=O_scale)
 
 
+        
+
+        
+        
+        
+        
+        
     def set_omega(self,risk_mat):
 
         self.qmat = []
@@ -60,7 +71,11 @@ class informs:
         for i in self.risk_sedol:
             sedol_var_list.append("d" + str(i))
 
+
+
         sedol_var_list.append("assum")
+        
+        sedol_var_list.append("O")
 
         for i in range(len(risk_mat)):
             qmat_1 = []
@@ -69,24 +84,33 @@ class informs:
             for j in risk_mat[i]:
                 new_risk_mat.append(j*self.omegamulti)
             new_risk_mat.append(0)
+            new_risk_mat.append(0)
             qmat_1.append(new_risk_mat)
             self.qmat.append(qmat_1)
 
-        for i in range(1):
+        for i in range(2):
             qmat_1 = []
             qmat_1.append(sedol_var_list)
             new_risk_mat = []
             for j in risk_mat[i]:
                 new_risk_mat.append(0)
             new_risk_mat.append(0)
+            new_risk_mat.append(0)
             qmat_1.append(new_risk_mat)
             self.qmat.append(qmat_1)
+            
+            
+        
 
         a = np.full((len(self.alpha), len(self.alpha)), self.omegamulti)
 
         self.risk_mat = risk_mat * a
         self.cplexs.quard_con(Q_con=self.Q_con,multiple=self.omegamulti)
         self.cplexs.quard_obj(qmat=self.qmat)
+
+#    def set_alpha(self,alphas):
+#        self.alpha = alphas
+
 
     def set_con(self,cons):
         self.conlist = cons
@@ -96,6 +120,8 @@ class informs:
     def solve(self,end_cond):
         mins_list = []
         mins_list2 = []
+
+
         rss = []
 
         self.returns = 0
@@ -105,9 +131,10 @@ class informs:
         for i in self.risk_sedol:
             w_up_dic.update({i: 0})
                    
-        self.cplexs.set_upsum(big_w_dic=w_up_dic, w_upsums=0)
+                                                                                      
         
         list_result = self.cplexs.solves()
+        self.cplexs.set_upsum(big_w_dic=w_up_dic, w_upsums=0)
 
         w_dic = list_result[0]
         d_dic = list_result[1]
@@ -120,13 +147,17 @@ class informs:
         for i in w_dic.keys():
             sum_min += min(w_dic[i], self.dic_bench[i])
 
+        print("sum_min : " + str(1 - sum_min))
         mins_list.append(1 - sum_min)
 
         active_share = 1 - sum_min
 
+        print("AC")
+        print(active_share)
+
         k = 0
 
-        for i in d_dic.keys():
+        for i in self.risk_sedol:
             mat_1 = np.append(mat_1, d_dic[i])
             mat_2[k] = np.array([d_dic[i]])
             k += 1
@@ -138,7 +169,11 @@ class informs:
 
         a = np.dot(mat_1, self.risk_mat)
         b = np.dot(a, mat_2)
-
+        c = np.dot(mat_1,a)
+        
+        print("TE")
+        print(c)
+        print(b)
         mins_list2.append(b)
 
         TE = b
@@ -164,10 +199,13 @@ class informs:
         final_result_dic = {}
 
         while (end):
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!bysta")
             if dist < end_cond:
                 if active_share < 0.6 or TE < 0.0025*self.omegamulti:
                     if Feasible_check == 0:
+                        print("No Solution")
                         return 0
+
                     else:
                         return final_result_dic
                 else:
@@ -193,7 +231,7 @@ class informs:
 
             k = 0
 
-            for i in d_dic.keys():
+            for i in self.risk_sedol:
                 mat_1 = np.append(mat_1, d_dic[i])
                 mat_2[k] = np.array([d_dic[i]])
                 k += 1
@@ -202,12 +240,20 @@ class informs:
             b = np.dot(a, mat_2)
             TE = b
 
+            print(TE)
+            print(active_share)
             if active_share < 0.6 or TE < 0.0025*self.omegamulti:
                 dist = w_upsum2 - w_upsum_in
                 w_upsum = w_upsum_in
             else:
                 dist = w_upsum_in - w_upsum
                 w_upsum2 = w_upsum_in
-                final_result_dic = list_result2
-                Feasible_check += 1
-
+                Feasible_check += 1              
+                if Feasible_check == 1:
+                    final_result_dic = list_result2
+                    chobj = list_result2[2]
+                if Feasible_check > 1:
+                    if chobj > list_result2[2]:
+                        final_result_dic = list_result2
+                        chobj = list_result2[2]
+                
